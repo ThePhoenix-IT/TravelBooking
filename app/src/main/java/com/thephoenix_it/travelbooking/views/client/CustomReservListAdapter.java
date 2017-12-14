2fae197caa7282bbf6a6ee90543e9fcc0ae5037c
hamzajg <hamzajg@gmail.com>
2017-12-15 06:03:19 +0900

    Add Notification....

--- app/src/main/java/com/thephoenix_it/travelbooking/views/client/CustomReservListAdapter.java
+++ app/src/main/java/com/thephoenix_it/travelbooking/views/client/CustomReservListAdapter.java
@@ -1,6 +1,12 @@
 package com.thephoenix_it.travelbooking.views.client;
 
 import android.app.Activity;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.support.v4.app.NotificationCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,7 +28,6 @@ public class CustomReservListAdapter extends ArrayAdapter {
     private final Activity context;
     private List<Reservation> reservationList;
     public CustomReservListAdapter(Activity context, List<Reservation> reservationList) {
-
         super(context, R.layout.reserv_listview_row , reservationList);
         this.context = context;
         this.reservationList = reservationList;
@@ -41,6 +46,21 @@ public class CustomReservListAdapter extends ArrayAdapter {
         if(reservationList.get(position) != null) {
             nameTextField.setText(reservationList.get(position).getVol().getDepart() + " - " + reservationList.get(position).getVol().getDestination());
             infoTextField.setText("" + reservationList.get(position).getEtatReservation().getDesc_etat());
+            if (position == reservationList.size() - 1 ) {
+                if(reservationList.get(position).getEtatReservation().getDesc_etat().equals("Confirmer")) {
+                    final Intent emptyIntent = new Intent();
+                    PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, emptyIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+                    NotificationCompat.Builder mBuilder =
+                            new NotificationCompat.Builder(context)
+                                    .setContentTitle("Confirmation")
+                                    .setContentText("Votre reservation pour le vol n° " + reservationList.get(position).getVol().getNum_vol() + " a ete confirmer!")
+                                    .setContentIntent(pendingIntent);
+                    mBuilder.setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION));
+                    NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+                    notificationManager.notify(0, mBuilder.build());
+                }
+            }
         }
         //imageView.setImageResource(imageIDarray[position]);
 
